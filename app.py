0326b7d93735733dc5c1c9334f5e41f9c441041f
DylanL <rex_ba@hotmail.com>
2021-04-07 16:51:20 +0800

    [New] WebSocket.

--- app.py
+++ app.py
@@ -1,11 +1,29 @@
-from flask import Flask
+from flask import Flask, render_template
+from flask_sockets import Sockets
 
 app = Flask(__name__)
+app.debug = True
+sockets = Sockets(app)
+
+
+@sockets.route('/echo')
+def echo_scoket(ws):
+    while True:
+        message = ws.receive()
+        if message == "socket open!":
+            ws.send("Start to use Chatbot.")
+        else:
+            ws.send(message)
 
 
 @app.route('/')
 def hello_world():
     return 'Hello World!'
+
+
+@app.route('/echo_test', methods=['GET'])
+def echo_test():
+    return render_template('index.html')
 
 
 if __name__ == '__main__':
